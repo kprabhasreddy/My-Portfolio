8fa792157a2c22d73d359ca0f81f3a10aed675f3
Prabhas Reddy Kasireddy <kprabhasreddy@gmail.com>
2025-07-06 20:38:56 -0400

    Initial commit: Prabhas Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
+
 export default defineConfig({
+  base: '/My-Portfolio/', // <- MUST match your repo name
   plugins: [react()],
 })
